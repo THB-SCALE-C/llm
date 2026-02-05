348e9e67eba672250bbcb52cc0ddb1861e672037
FabSchlag95 <fabianschlagheck@mail.de>
2026-02-05 15:55:22 +0100

    fix

--- llm/retrieval/vector_similarity.py
+++ llm/retrieval/vector_similarity.py
@@ -1,17 +1,20 @@
-from llm.lib.supabase import query_vector_db
+from supabase import Client
+from llm.lib.supabase import get_supabase, query_vector_db
 from llm.provider import get_embedding_model
 
 
 def retrieve_by_similarity(queries: list[str] | str, 
                            embedding_provider: str = "openrouter", 
                            embedding_model: str = "sentence-transformers/all-minilm-l12-v2", 
-                           document_ids: list[int] | None = None, match_count=5):
+                           document_ids: list[int] | None = None,
+                            client:Client = get_supabase(),
+                             match_count=5):
     model = get_embedding_model(embedding_provider, embedding_model)
     if isinstance(queries, str):
         query_embeddings = model.embed([queries])
         query_vector = query_embeddings[0].vector
         results = query_vector_db(query_embedding=query_vector,
-                        document_ids=document_ids, match_count=match_count,)
+                        document_ids=document_ids, match_count=match_count,client=client)
     else:
         results = []
         query_embeddings = model.embed(queries)
